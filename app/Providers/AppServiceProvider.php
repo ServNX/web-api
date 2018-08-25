cd26f01200003a0de96d28133b2b9bc46884a608
poppabear8883 <servnx@gmail.com>
2018-08-25 01:11:19 -0400

    api(Container): Register bindings in service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Contracts\ServiceRepositoryInterface;
+use App\Contracts\UserRepositoryInterface;
+use App\Repositories\ServiceRepository;
+use App\Repositories\UserRepository;
+use App\Services\GithubService;
+use Github\Client as GithubClient;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +29,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(ServiceRepositoryInterface::class, ServiceRepository::class);
+
+        /**
+         * Services
+         */
+        $this->registerGithubService();
     }
+
+    /**
+     * Register the github service class.
+     *
+     * @return void
+     */
+    protected function registerGithubService()
+    {
+        $this->app->singleton('service.github', function () {
+            return new GithubService(new GithubClient());
+        });
+
+        $this->app->alias('service.github', GithubService::class);
+    }
+
 }
