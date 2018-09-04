89e870d3bba989e3a42e9e0faba7ae10f9ef56c2
poppabear8883 <servnx@gmail.com>
2018-09-04 19:03:26 -0400

    api: Resource collection mapping indexes

--- app/Services/Github/Api/Repositories.php
+++ app/Services/Github/Api/Repositories.php
@@ -3,26 +3,16 @@
 namespace App\Services\Github\Api;
 
 use App\Services\Github\Contracts\RepositoriesInterface;
-use App\Services\Repository;
 
 class Repositories extends AbstractApi implements RepositoriesInterface
 {
     public function all()
     {
-        $collection = collect([]);
-        $repos = $this->github->api('user')->repositories($this->username);
+        $data = $this->github->api('user')->repositories($this->username);
 
-        foreach ($repos as $repo) {
-            $collection->push(new Repository(
-                $repo['name'],
-                $repo['open_issues'],
-                $repo['forks'],
-                $repo['stargazers_count'],
-                $repo['watchers']
-            ));
-        }
-
-        return $collection;
+        return $this->resourceCollection($data, [
+            'stars' => 'stargazers_count'
+        ]);
     }
 
     public function create($name)
@@ -48,5 +38,37 @@ class Repositories extends AbstractApi implements RepositoriesInterface
     public function findById($id)
     {
         // TODO: Implement findById() method.
+    }
+
+    protected function resourceCollection(array $data, array $map = [])
+    {
+        $collection = collect([]);
+
+        foreach ($data as $d) {
+            $collection->push([
+
+                'name' => array_key_exists('name', $map)
+                    ? $d[$map['name']]
+                    : $d['name'],
+
+                'open_issues' => array_key_exists('open_issues', $map)
+                    ? $d[$map['open_issues']]
+                    : $d['open_issues'],
+
+                'forks' => array_key_exists('forks', $map)
+                    ? $d[$map['forks']]
+                    : $d['forks'],
+
+                'stars' => array_key_exists('stars', $map)
+                    ? $d[$map['stars']]
+                    : $d['stars'],
+
+                'watchers' => array_key_exists('watchers', $map)
+                    ? $d[$map['watchers']]
+                    : $d['watchers'],
+            ]);
+        }
+
+        return $collection;
     }
 }
